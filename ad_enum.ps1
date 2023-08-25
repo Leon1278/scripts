c162b8d8d00088642d11900a34b04e187770cfd1
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-08-25 07:58:21 -0400

    Create ad_enum.ps1

---
+++ ad_enum.ps1
@@ -0,0 +1,8 @@
+# Store the domain object in the $domainObj variable
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+# Store the PdcRoleOwner name to the $PDC variable
+$PDC = $domainObj.PdcRoleOwner.Name
+
+# Print the $PDC variable
+$PDC
