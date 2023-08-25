cc7edd2ed83e0585b5076feda887a47c267b8bca
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-08-25 08:00:14 -0400

    Update ad_enum.ps1

--- ad_enum.ps1
+++ ad_enum.ps1
@@ -1,8 +1,13 @@
+#Hint: Before execution we maybe must bypass the execution policy. Therefore execute: "powershell -ep bypass" in terminal
+
 # Store the domain object in the $domainObj variable
 $domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 
 # Store the PdcRoleOwner name to the $PDC variable
 $PDC = $domainObj.PdcRoleOwner.Name
 
-# Print the $PDC variable
-$PDC
+# Store the Distinguished Name variable into the $DN variable
+$DN = ([adsi]'').distinguishedName
+
+# Print the $DN variable
+$DN
