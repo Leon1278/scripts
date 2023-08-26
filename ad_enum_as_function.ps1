2e31abae604ae6c46124880c4088434454398422
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-08-26 05:16:36 -0400

    Update ad_enum_as_function.ps1

--- ad_enum_as_function.ps1
+++ ad_enum_as_function.ps1
@@ -1,3 +1,5 @@
+#Hint: Before execution we maybe must bypass the execution policy. Therefore execute: "powershell -ep bypass" in terminal
+
 #Hint: Import this function in powershell to be able to use it as cmdlet -> 'Import-Module .\ad_enum_as_function.ps1'
 # We can now query using the function LDAPSearch -> 'LDAPSearch -LDAPQuery "(samAccountType=805306368)"'
 #                                                -> 'LDAPSearch -LDAPQuery "(objectclass=group)"'
