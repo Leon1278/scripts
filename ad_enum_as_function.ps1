6132a426140d9b7ab3a69f8a1efe8ccbfcb6f7dc
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-08-26 05:11:38 -0400

    Update ad_enum_as_function.ps1

--- ad_enum_as_function.ps1
+++ ad_enum_as_function.ps1
@@ -1,6 +1,8 @@
 #Hint: Import this function in powershell to be able to use it as cmdlet -> 'Import-Module .\ad_enum_as_function.ps1'
 # We can now query using the function LDAPSearch -> 'LDAPSearch -LDAPQuery "(samAccountType=805306368)"'
 #                                                -> 'LDAPSearch -LDAPQuery "(objectclass=group)"'
+
+# We can store query results into a variable an then access different properties
 #                                                -> '$group = LDAPSearch -LDAPQuery "(&(objectCategory=group)(cn=Development Department*))"' -> '$group.properties.member'
 
 function LDAPSearch {
